dedc435b8e875a4baf9dbdf079f959ff2fe523f2
seongyurim <miruyseong@gmail.com>
2023-04-12 17:30:48 +0900

    first commit

--- RealApple_clone/main_original.js
+++ RealApple_clone/main_original.js
@@ -15,20 +15,18 @@
 
     ////// 섹션의 정보를 배열 속 오브젝트에 생성 -----------------------------------------------
     const sectionSet = [
-        // section-0
+        // section-0의 정보
         {
-            height: 0,
+            height : 0,
             hMultiple : 5,
             objs : {container : document.querySelector("#section-0")}
-
         },
 
-        // section-1
+        // section-1의 정보
         {
-            height: 0,
-            hMultiple : 3,
+            height : 0,
+            hMultiple : 4,
             objs : {container : document.querySelector("#section-1")}
-
         }
     ];
 
@@ -37,37 +35,42 @@
     const setLayout = function() {
 
         let height = 0;
-
+        
+        // 높이가 500px보다 작은 경우는 강제로 500으로 설정한다.
         if (window.innerHeight < 500) {
-
+            
             height = 500;
-
+            
         } else {
-
+            
             height = window.innerHeight;
-
+            
+            
         }
-
+        
+        // 각 섹션의 높이를 설정한다.
         for (let i = 0; i < sectionSet.length; i++) {
 
             sectionSet[i].height = height * sectionSet[i].hMultiple;
             sectionSet[i].objs.container.style.height = `${sectionSet[i].height}px`;
-
+            
         }
-
-    };
-
+    }    
+    // 지금 이 위치에서 이 함수가 일회성으로 setLayout(); 호출되면 생기는 문제?
+    // 특정 사이즈로 보다가 사용자가 브라우저 크기를 변경했을 경우
+    // 그 바뀐 사이즈로 바로 바뀌지 않고, 새로고침을 해야만 변경된다.
+    // 따라서 크기가 변경될 때마다 함수가 호출되어야 하겠다.
 
 
     /////// 스크롤된 시점의 섹션명을 도출하는 함수 생성 -------------------------------------------------
-    const getCurrentSection = function() {
+    const getCurrentSection = function(){
 
         let segment = [
-
             sectionSet[0].height,
             sectionSet[0].height + sectionSet[1].height
-
         ];
+        // 지금은 섹션이 2개라서 특정 스크롤 숫자를 넘냐/안넘냐의 차이라 배열이 굳이 필요 없지만
+        // 섹션이 4개라면 특정 스크롤 숫자가 3개가 필요하기 때문에 이럴 때에는 배열을 사용하는 것이 좋다.
 
         if (yOffset <= segment[0]) {
 
@@ -79,27 +82,41 @@
 
         } else {
 
-            // 그럴 일은 없다.
+            // 올 일이 없다.
             console.error("[ERROR] getCurrentSection()");
-
         }
 
-    };
+        return section;
+
+    }
+    
+
 
 
     /////// EVENT LISTENER ////////////////////////////////////////////////////////////////////////////////////
-    
-    ////// 스크롤 될 때마다 값을 도출하는 이벤트
+
+
+
 
     window.addEventListener('scroll', () => {
 
         yOffset = window.scrollY;
         currentSection = getCurrentSection();
+        // console.log(`yOffset = ${yOffset}, section = ${currentSection}`);
 
     });
+    
 
+    // 결국 setLayout() 호출을 위해 두가지 이벤트를 추가해야 한다.
+    // 1) 처음 로딩될 때 setLayout()를 호출해준다.
+    window.addEventListener('load', () => {
+        setLayout();
+    });
 
-
+    // 2) 사이즈가 변경되면 setLayout() 다시 호출해준다.
+    window.addEventListener('resize', () => {
+        setLayout();
+    });
 
 
 
