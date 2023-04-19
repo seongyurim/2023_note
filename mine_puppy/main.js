28d734f14bb44fa2d97322098a4fa114c220c18c
seongyurim <miruyseong@gmail.com>
2023-04-19 18:53:05 +0900

    first commit

--- mine_puppy/main.js
+++ mine_puppy/main.js
@@ -78,14 +78,60 @@
     }
 
 
+    ////// 로컬 내비게이션을 특정 시점부터 고정 ---------------------------------------------------------------
+    const makeLocalNavFixed = function() {
+
+        if (yOffset > 44) {
+
+            // 고정시키기
+            document.body.classList.add('local_nav_fixed');
+
+        } else {
+
+            // 되돌리기
+            document.body.classList.remove('local_nav_fixed');
+
+        }
+
+    }
+
+
 
 
     ////// 이벤트리스너 ////////////////////////////////////////////////////////////////
 
+    window.addEventListener('scroll', () => {
+
+        // 스크롤값 설정
+        yOffset = window.scrollY;
+
+        // 로컬 내비게이션을 특정 위치부터 고정
+        makeLocalNavFixed();
+
+    });
+
+
     window.addEventListener('load', () => {
 
+        // 레이아웃 설정
         setLayout();
 
+        // 스크롤값 설정
+        yOffset = window.scrollY;
+
+        // 로컬 내비게이션을 특정 위치부터 고정
+        makeLocalNavFixed();
+
+    });
+
+
+    window.addEventListener('resize', () => {
+
+        // 레이아웃 재설정
+        setLayout();
+
+        // CSS 변경
+        makeLocalNavFixed();
 
     });
 
