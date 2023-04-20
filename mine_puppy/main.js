0d92ab80b3046842357f365fc2a2f69ba3236560
seongyurim <miruyseong@gmail.com>
2023-04-20 17:08:28 +0900

    first commit

--- mine_puppy/main.js
+++ mine_puppy/main.js
@@ -78,16 +78,112 @@
     }
 
 
+    ////// 로컬 내비게이션을 특정 시점부터 고정 ---------------------------------------------------------------
+    const makeLocalNavFixed = function() {
+
+        if (yOffset > 44) {
+
+            // 고정시키기
+            document.body.classList.add('local_nav_fixed');
+
+        } else {
+
+            // 되돌리기
+            document.body.classList.remove('local_nav_fixed');
+
+        }
+
+    }
+
+    ////// 섹션0의 타이틀에 트랜지션 부여 ---------------------------------------------------------------------
+    const setTitleTransY = function() {
+
+        const $titleTransY = document.querySelector(".sec0_txt_inner");
+        $titleTransY.setAttribute('class', 'sec0_txt_inner_transY');
+
+    }
+
+
+    ////// 화살표 버튼을 클릭하면 최상단으로 스크롤 -----------------------------------------------------------
+    const $scrollBtn = document.querySelector('.scrollToTop');
+    const backToTop = function() {
+        
+        window.scrollTo({
+            top: 0,
+            left: 0,
+            behavior: 'smooth'
+        });
+
+    }
+
+    ////// 화살표 버튼이 섹션0(탑)에 있을 때만 안보이게 --------------------------------------------------------
+    const HideScrollBtn = function() {
+
+        if (yOffset > 0) {
+
+            $scrollBtn.style.opacity = 1;
+
+        } else {
+
+            $scrollBtn.style.opacity = 0;
+
+        }
+
+    }
+
+
+
+
+
+
+
 
 
     ////// 이벤트리스너 ////////////////////////////////////////////////////////////////
 
-    window.addEventListener('load', () => {
+    window.addEventListener('scroll', () => {
 
-        setLayout();
+        // 스크롤값 설정
+        yOffset = window.scrollY;
 
+        // 로컬 내비게이션을 특정 위치부터 고정
+        makeLocalNavFixed();
+
+        // 스크롤 버튼이 섹션0(탑)에서만 안보이게
+        HideScrollBtn();
 
     });
+
+
+    window.addEventListener('load', () => {
+
+        // 레이아웃 설정
+        setLayout();
+
+        // 스크롤값 설정
+        yOffset = window.scrollY;
+
+        // 로컬 내비게이션을 특정 위치부터 고정
+        makeLocalNavFixed();
+
+        setTitleTransY();
+
+    });
+
+
+    window.addEventListener('resize', () => {
+
+        // 레이아웃 재설정
+        setLayout();
+
+        // CSS 변경
+        makeLocalNavFixed();
+
+    });
+
+    // 화살표 버튼을 클릭하면 최상단으로 스크롤
+    $scrollBtn.addEventListener('click', backToTop);
+    
 
 
 
